bbf6da3f47ac5cd7a6dbd079de7e22a10781dd19
ajith <ajithzres@gmail.com>
2020-02-06 12:03:16 +0000

    include nos kernel and build gapps

--- aosp_oneplus3.mk
+++ aosp_oneplus3.mk
@@ -22,7 +22,7 @@ $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
 # Inherit some common AOSP stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
+TARGET_GAPPS_ARCH := arm64
 # Vendor security patch level
 VENDOR_SECURITY_PATCH := 2019-10-01
 
